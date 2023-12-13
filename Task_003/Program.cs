c1efc82f37e9cce2a07f8f9a637bef7d49443756
Vlad Aver <averyanau19gmail.com>
2023-12-13 20:53:15 +0100

    Добавил решение третьей задачи

---
+++ Task_003/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задайте произвольный массив. Выведете его элементы, начиная с конца. 
+// Использовать рекурсию, не использовать циклы.
+
+int arraySize = ReadInt("Введите колличество элементов в массиве: ");
+int[] array = RandomArray(arraySize);
+Console.WriteLine("Исходный массив:");
+PrintArray(array);
+Console.WriteLine("\nМассив, начиная с конца:");
+ArrayReversed(array, array.Length - 1);
+
+//-------------------------------------
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+static int[] RandomArray(int size)
+{
+    Random rand = new Random();
+    int[] array = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rand.Next(1, 100);
+    }
+
+    return array;
+}
+
+static void PrintArray(int[] array)
+{
+    foreach (var element in array)
+    {
+        Console.Write(element + " ");
+    }
+}
+
+static void ArrayReversed(int[] array, int index)
+{
+    if (index < 0)
+    {
+        return;
+    }
+    Console.Write(array[index] + " ");
+    ArrayReversed(array, index - 1);
+}
+
+
